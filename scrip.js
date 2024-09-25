3ff41a2918587c251c4c41e26778f44a37bed09f
NicoDuartex <nicoduarte2004@gmail.com>
2024-09-25 10:55:10 -0300

    Version4

--- scrip.js
+++ scrip.js
@@ -1,8 +1,5 @@
 // JavaScript para manejar la lógica del cálculo
-console.log("Archivo JavaScript cargado"); // Verifica que se carga correctamente
-
 document.getElementById('calcular').addEventListener('click', function () {
-    console.log("Botón Calcular presionado"); // Verifica que se ejecuta
     const form = document.forms['presupuestoForm'];
     let subtotal = 0;
 
@@ -29,8 +26,35 @@ document.getElementById('calcular').addEventListener('click', function () {
     document.getElementById('valorCuota18').textContent = cuota18.toFixed(2);
 });
 
-// Evento para imprimir presupuesto
 document.getElementById('imprimir').addEventListener('click', function () {
-    console.log("Botón Imprimir presionado"); // Verifica que se ejecuta
     window.print();
+});
+
+document.getElementById('exportarPdf').addEventListener('click', function () {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+    doc.setFontSize(18);
+    doc.text("Presupuestos mecatrónica", 10, 10);
+    const form = document.forms['presupuestoForm'];
+    for (let i = 1; i <= 5; i++) {
+        const cantidad = form[`cantidad${i}`].value;
+        const precioUnitario = form[`precioUnitario${i}`].value;
+        const precioTotal = cantidad * precioUnitario;
+        doc.text(`Cantidad: ${cantidad}`, 10, i*22 );
+        doc.text(`Precio Unitario: ${precioUnitario}`, 50, i*22);
+        doc.text(`Precio Total: ${precioTotal}`, 110 , i*22);
+    }
+    const subtotal = document.getElementById('subtotal').textContent;
+    const iva = document.getElementById('iva').textContent;
+    const total = document.getElementById('total').textContent;
+    const totalAhora12 = document.getElementById("valorCuota12").textContent;
+    const totalAhora18 = document.getElementById("valorCuota18").textContent;
+
+    doc.text(`Subtotal: ${subtotal}`, 10, 280);
+    doc.text(`IVA: ${iva}`, 10, 270);
+    doc.text(`Total: ${total}`, 10, 290);
+    doc.text(`Ahora12: ${totalAhora12}`, 10, 250);
+    doc.text(`Ahora18: ${totalAhora18}`, 10, 260);
+
+    doc.save('PresupuestoMecatronica.pdf');
 });
